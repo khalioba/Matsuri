85388110098ac5d7324bae3b7891ae1648900c84
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-06-12 23:46:03 +0100

    test

--- inscription.php
+++ inscription.php
@@ -3,5 +3,12 @@ include('include/inscription_header.php')
 ?>
 
 <div class="welcome_text space">
-    <center><p>Vous êtes le Bienvenue👋 sur Matsuri</p></center>
+    <center>
+        <p class="welcom_text1">Vous êtes le Bienvenue👋 sur Matsuri</p>
+        <p class="welcom_text2">Le site pour acheter les tickets et se faire plaisir</p>
+    </center>
+</div>
+
+<div class="">
+
 </div>
