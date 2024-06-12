0aadb6e890b8963e79dd5e7ddc52f0c249ac359b
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-06-12 19:31:47 +0100

    page

---
+++ include/inscription_header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Matsuri</title>
+</head>
+
+<body>
+
+    <header>
+        <nav class="nav_bar space">
+            <div class="logo_container">
+                <a href="index.php">
+                    <img src="image/logo.png" alt="logo Matsuri">
+                    <p>Matsuri</p>
+                </a>
+            </div>
+
+            <ul class="menu_nav space" id="menu_nav">
+                <li class="btn_nav"><a href="index.php">Accueil</a></li>
+                <li class="btn_nav"><a href="service.php">Services</a></li>
+                <li class="btn_nav"><a href="galerie.php">Galerie</a></li>
+                <li class="btn_nav"><a href="index.php#about">A propos</a></li>
+                <li class="btn_nav"><a href="contact.php">Contact</a></li>
+            </ul>
+
+            <div class="menu_hamburger" id="hamburger">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </div>
+        </nav>
+    </header>
+
+</body>
+
+</html>
