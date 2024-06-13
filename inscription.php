0474612b74a17d41e3216ccb125b179eedaac8fb
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-06-13 14:30:01 +0100

    page d'inscription telephone done

--- inscription.php
+++ inscription.php
@@ -1,14 +1,30 @@
 <?php
 include('include/inscription_header.php')
 ?>
+<br>
+<center>
+    <div class="welcome_text space">
 
-<div class="welcome_text space">
-    <center>
         <p class="welcom_text1">Vous êtes le Bienvenue👋 sur Matsuri</p>
-        <p class="welcom_text2">Le site pour acheter les tickets et se faire plaisir</p>
-    </center>
-</div>
+        <br>
+        <p class="welcom_text2 space">Le site pour acheter les tickets et se faire plaisir</p>
+        <br>
+        <br>
+        <br>
+        <p class="inscription_text space">Pour commencer votre aventure, inscrivez-vous en entrant votre numéro de
+            téléphone</p>
+        <br>
+    </div>
 
-<div class="">
+    <div class="informations space">
+        <form id="contactForm">
+            <div class="inscription_box">
+                <input class="prenom" type="text" name="prenom" id="nom" placeholder="Numéro de téléphone" required>
+            </div>
+            <br>
+            <input type="submit" value="Recevoir le code">
+        </form>
 
-</div>
+    </div>
+
+</center>
