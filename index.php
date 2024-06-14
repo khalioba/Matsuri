0a03ebc23a60f5866ccb23a95f5707b556eb20c0
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-06-14 15:03:44 +0100

    home page

--- index.php
+++ index.php
@@ -45,6 +45,140 @@ include('include/header.php')
     <!-- If we need scrollbar -->
     <!-- <div class="swiper-scrollbar"></div> -->
 </div>
+<br>
+
+
+<div class="programme_bloc">
+    <a href="programme.php">
+        <div class="nos_programme_title arrow space2">
+            <p class="form_bf_title programme_title">
+                Ce mois-ci
+            </p>
+            <img src="image/arrow_white.svg" alt="">
+        </div>
+    </a>
+    <div class="all_programme">
+        <div class="programme_carre">
+            <img src="image/concert_carre1.jpg" alt="Avatar">
+        </div>
+        <div class="programme_carre">
+            <img src="image/concert_carre3.jpg" alt="Avatar">
+        </div>
+        <div class="programme_carre">
+            <img src="image/concert_carre4.jpg" alt="Avatar">
+        </div>
+        <div class="programme_carre">
+            <img src="image/concert_carre5.webp" alt="Avatar">
+        </div>
+        <div class="programme_carre">
+            <img src="image/concert_carre6.png" alt="Avatar">
+        </div>
+        <div class="programme_carre">
+            <img src="image/concert_carre7.jpeg" alt="Avatar">
+        </div>
+        <div class="programme_carre">
+            <img src="image/concert_carre8.webp" alt="Avatar">
+        </div>
+        <div class="programme_carre">
+            <img src="image/concert_carre9.png" alt="Avatar">
+        </div>
+        <div class="programme_carre">
+            <img src="image/concert_carre5.webp" alt="Avatar">
+        </div>
+        <div class="programme_carre">
+            <img src="image/concert_carre10.png" alt="Avatar">
+        </div>
+    </div>
+</div>
+
+<div class="programme_bloc">
+    <a href="programme.php">
+        <div class="nos_programme_title arrow space2">
+            <p class="form_bf_title programme_title">
+                Avril
+            </p>
+            <img src="image/arrow_white.svg" alt="">
+        </div>
+    </a>
+    <div class="all_programme">
+        <div class="programme_rectangle">
+            <img src="image/event_rectangle1.jpg" alt="Avatar">
+        </div>
+        <div class="programme_rectangle">
+            <img src="image/event_rectangle4.jpg" alt="Avatar">
+        </div>
+        <div class="programme_rectangle">
+            <img src="image/event_rectangle5.jpg" alt="Avatar">
+        </div>
+        <div class="programme_rectangle">
+            <img src="image/event_rectangle2.jpg" alt="Avatar">
+        </div>
+        <div class="programme_rectangle">
+            <img src="image/event_rectangle3.jpg" alt="Avatar">
+        </div>
+        <div class="programme_rectangle">
+            <img src="image/event_rectangle1.jpg" alt="Avatar">
+        </div>
+        <div class="programme_rectangle">
+            <img src="image/event_rectangle2.jpg" alt="Avatar">
+        </div>
+        <div class="programme_rectangle">
+            <img src="image/event_rectangle4.jpg" alt="Avatar">
+        </div>
+        <div class="programme_rectangle">
+            <img src="image/event_rectangle5.jpg" alt="Avatar">
+        </div>
+        <div class="programme_rectangle">
+            <img src="image/event_rectangle3.jpg" alt="Avatar">
+        </div>
+    </div>
+</div>
+
+<div class="programme_bloc">
+    <a href="programme.php">
+        <div class="nos_programme_title arrow space2">
+            <p class="form_bf_title programme_title">
+                Avril
+            </p>
+            <img src="image/arrow_white.svg" alt="">
+        </div>
+    </a>
+    <div class="all_programme">
+        <div class="programme_portrait">
+            <img src="image/event_portrait1.jpg" alt="Avatar">
+        </div>
+        <div class="programme_portrait">
+            <img src="image/event_portrait2.jpg" alt="Avatar">
+        </div>
+        <div class="programme_portrait">
+            <img src="image/event_portrait3.jpg" alt="Avatar">
+        </div>
+        <div class="programme_portrait">
+            <img src="image/event_portrait4.jpg" alt="Avatar">
+        </div>
+        <div class="programme_portrait">
+            <img src="image/event_portrait5.jpg" alt="Avatar">
+        </div>
+        <div class="programme_portrait">
+            <img src="image/event_portrait6.jpg" alt="Avatar">
+        </div>
+        <div class="programme_portrait">
+            <img src="image/event_portrait2.jpg" alt="Avatar">
+        </div>
+        <div class="programme_portrait">
+            <img src="image/event_portrait4.jpg" alt="Avatar">
+        </div>
+        <div class="programme_portrait">
+            <img src="image/event_portrait3.jpg" alt="Avatar">
+        </div>
+        <div class="programme_portrait">
+            <img src="image/event_portrait6.jpg" alt="Avatar">
+        </div>
+    </div>
+</div>
+
+
+
 
 
 
