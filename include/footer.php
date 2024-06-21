8f04ddb39b40960da8618ff8fea47f79b84e2702
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-06-21 15:03:22 +0100

    footer added

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,71 @@
+<div class="footer">
+    <div class="footer_items wow animate__animated animate__fadeInUp animated">
+        <div class="footer_item_left">
+            <h4>Nous <span class="text_color">suivre</span></h4>
+            <div class="footer_icons">
+                <a href="https://www.facebook.com/profile.php?id=100064104010472">
+                    <div class="circle facebook">
+                        <img src="image/facebook.svg" alt="">
+                    </div>
+                </a>
+
+                <a href="https://www.instagram.com/martincoiffure/">
+                    <div class="circle insta">
+                        <img src="image/instagram.svg" alt="">
+                    </div>
+                </a>
+
+                <a href="https://www.tiktok.com/@martincoiffure">
+                    <div class="circle tiktok">
+                        <img src="image/tiktok.svg" alt="">
+                    </div>
+                </a>
+
+            </div>
+        </div>
+        <div class="footer_item_center">
+            <h4>Nous <span class="text_color">Contacter</span></h4>
+            <div class="footer_icons_column">
+                <div class="contacter">
+                    <div class="circle call">
+                        <img src="image/call.svg" alt="">
+                    </div>
+                    <div class="contacter_number">
+                        <p>+242 06 600 00 00</p>
+                        <p>05 500 00 00</p>
+                    </div>
+                </div>
+                <div class="contacter">
+                    <a
+                        href="https://api.whatsapp.com/send?phone=%2B242066612033&context=ARD89EEUoSuYa4Ep1E6d3gR7OP0VIaWKMnGLahXn03sqJqUi1m7ifAZrP6Gw7Rrmo3O1PTG21mWr5_QhDtOSUGUWPwV8oAASo_2HLcM-OUHFx2Q2IeTWYH4h5Uy-vKJ7EhHqAAOQH0q9MR4ZlEmunLAA8A&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwZXh0bgNhZW0CMTAAAR3CBzjaOVj945WrjvqW4WqZojPNCgxKAIed4Ts_AyRcxvxYs9UsFi77gVk_aem_AWaR-McLahJtN6JqKqC48CcTh2r7rY6K6ZZdiS-8CykcwfM1DxYADU9PxavMoUcGPywa-LmXY_ncxDkGaZrwpv5J">
+                        <div class="circle whatsapp">
+                            <img src="image/whatsapp.svg" alt="">
+                        </div>
+                        <div class="contacter_number">
+                            <p>+242 06 600 00 00</p>
+                        </div>
+                    </a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="copyright_powered">
+    <div class=" copyright">
+        <p>Copyright © 2023 Matsuri</p>
+    </div>
+    <div class="powered">
+        <p>Développé par Rêveurs professionnels & yashinka</p>
+    </div>
+</div>
+
+
+
+
+
+
 <!-- swiper script -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
@@ -11,7 +79,7 @@ const swiper = new Swiper('.swiper', {
 
     // Autoplay
     autoplay: {
-        delay: 2500, // délai entre chaque diapositive (en millisecondes)
+        delay: 5000000, // délai entre chaque diapositive (en millisecondes)
         disableOnInteraction: false, // le défilement automatique ne s'arrête pas lors de l'interaction utilisateur
     },
 
