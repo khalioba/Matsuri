82702d7c30418223cfce2adb1383e21f64b0cfcb
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-06-23 00:12:22 +0100

    buy ticket button done

--- eventfree.php
+++ eventfree.php
@@ -9,22 +9,42 @@ include('include/header.php')
 
 
 <center>
-    <div class="visuel_title space2">
+    <div class="visuel_title space2 space_bottom">
         <h1>Festival du mangas d'été</h1>
     </div>
 </center>
 
-<div class="event_date space2">
+<div class="event_date space2 space_bottom">
     <div class="event_date_items">
         <div class="event_date_top">
             <div class="event_date_calendar">
                 <img src="image/calendar.svg" alt="">
+                <p>17 juin 2024 05h30</p>
             </div>
-
+            <div class="event_type">
+                <p>Evènement gratuit</p>
+            </div>
         </div>
-        <div class="event_date_bottom">
-
+        <br>
+        <div class="event_date_placeholder">
+            <img src="image/calendar.svg" alt="">
+            <p>41 rue Raymond Paillet, Bacongo, Brazzaville</p>
         </div>
     </div>
-
 </div>
+
+<div class="event_about space2 space_bottom">
+    <h3>A Propos</h3>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed,
+        dolor.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </p>
+</div>
+
+<center>
+    <div class="buy_button space_bottom">
+        <button>Prendre un ticket</button>
+    </div>
+
+</center>
